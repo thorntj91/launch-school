d8aad1229087ba42fd716029a7eeaa4289438d17
Jake Thornton <jake@thornton.eu.com>
2016-02-19 18:01:40 +0000

    Refactor calculator.rb and add improved number validation

--- programming_foundations/lesson_2/calculator.rb
+++ programming_foundations/lesson_2/calculator.rb
@@ -1,23 +1,68 @@
-def perform_op(operator, operands)
-  case operator
-  when '*'
-    puts operands[0] * operands[1]
-  when '/'
-    puts operands[0].to_f / operands[1].to_f
-  when '-'
-    puts operands[0] - operands[1]
-  when '+'
-    puts operands[0] + operands[1]
-  end
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-operands = []
+def integer?(num)
+  /^\d+$/.match(num)
+end
 
-puts 'Please enter first number'
-operands << gets.chomp.to_i
-puts 'Now, please enter the second number'
-operands << gets.chomp.to_i
-puts 'Now, please enter an operation to perform on these numbers'
-operator = gets.chomp
+def float?(num)
+  num.to_f.to_s == num
+end
 
-perform_op(operator, operands)
+def number?(num)
+  float?(num) || integer?(num)
+end
+
+operand1 = ""
+operand2 = ""
+operator = ""
+
+loop do
+  prompt("Welcome to Calculator")
+
+  loop do
+    prompt("Please enter first number")
+    operand1 = gets.chomp
+
+    break if number?(operand1)
+
+    prompt("Doesn't seem like a valid number")
+  end
+
+  loop do
+    prompt("Now, please enter the second number")
+    operand2 = gets.chomp
+
+    break if number?(operand2)
+
+    prompt("Doesn't seem like a valid number")
+  end
+
+  loop do
+    prompt("Now, please enter an operation to perform on these numbers")
+    operator = gets.chomp
+
+    break if %w(+ - / *).include? operator
+
+    prompt("Invalid operator. Try: +, -, *, /")
+  end
+
+  result =
+    case operator
+    when '*'
+      operand1.to_i * operand2.to_i
+    when '/'
+      operand1.to_f / operand2.to_f
+    when '-'
+      operand1.to_i - operand2.to_i
+    when '+'
+      operand1.to_i + operand2.to_i
+    end
+
+  prompt("This is the result: #{result}")
+
+  prompt("Do you want to calculate again?")
+
+  break unless gets.chomp.include?("y")
+end # main loop
