845baf326cff51f19cd06a189ba0b3597c4014e3
Jake Thornton <jake@thornton.eu.com>
2016-03-04 20:30:44 +0000

    Finish RPS bonus features

--- programming_foundations/lesson_2/rock_paper_scissors.rb
+++ programming_foundations/lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,27 @@
-VALID_CHOICES = %w(rock paper scissors).freeze
+VALID_CHOICES = { r: 'rock', p: 'paper', s: 'scissors', l: 'lizard',
+  sp: 'spock' }.freeze
+
+score = 0 
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  (first == 'rock') && (second == 'scissors' || second == 'lizard') ||
+    (first == 'paper') && (second == 'rock' || second == 'spock') ||
+    (first == 'scissors') && (second == 'paper' || second == 'lizard') ||
+    (first == 'spock') && (second == 'scissors' || second == 'rock') ||
+    (first == 'lizard') && (second == 'spock' || second == 'paper')
+end
+
+def update_score(player, computer, score)
+  score += 1 if win?(player, computer)
+  
+  if win?(computer, player)
+    score -= 1 unless score == 0
+  end
+  score
 end
 
 def display_results(player, computer)
@@ -23,22 +37,30 @@ end
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt('Choose one of the following: ')
+    choices = VALID_CHOICES.map{|key, value| "#{value} (#{key})"}.join(', ')
+    prompt(choices)
     choice = gets.chomp
 
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.has_key?(choice.to_sym)
+      choice = VALID_CHOICES[choice.to_sym]
+      break
+    elsif VALID_CHOICES.has_value?(choice)
       break
     else
       prompt("That's not a valid choice.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.values.sample
 
-  prompt("You choice: #{choice}; Computer chose: #{computer_choice}")
+  prompt("Your choice was: #{choice}; The computer chose: #{computer_choice}")
+
+  score = update_score(choice, computer_choice, score)
 
   display_results(choice, computer_choice)
 
+  prompt("Current score: #{score}")
   prompt("Do you want to play again?")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
